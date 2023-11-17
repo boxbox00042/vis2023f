eb2376ca6c2b8c15263c605fff8028e3c34f4c1f
boxbox00042 <39685185+boxbox00042@users.noreply.github.com>
2023-11-17 19:09:30 +0800

    111598050

--- hw02/src/simple/README.md
+++ hw02/src/simple/README.md
@@ -1,6 +1,6 @@
 # HW2 Simple baseline (4pt)
 
-https://observablehq.com/d/5e0c4e3fb8df662c@24
+https://observablehq.com/d/5e0c4e3fb8df662c@41
 
 View this notebook in your browser by running a web server in this folder. For
 example:
@@ -14,7 +14,7 @@ import this module directly into your application. To npm install:
 
 ~~~sh
 npm install @observablehq/runtime@5
-npm install https://api.observablehq.com/d/5e0c4e3fb8df662c@24.tgz?v=3
+npm install https://api.observablehq.com/d/5e0c4e3fb8df662c@41.tgz?v=3
 ~~~
 
 Then, import your notebook and the runtime as:
--- hw02/src/simple/data.js
+++ hw02/src/simple/data.js
@@ -7,26 +7,26 @@ function _data(FileAttachment){return(
 FileAttachment("data.json").json()
 )}
 
-function _settings(Inputs){return(
+function _plot1(Inputs){return(
 Inputs.form({
-	mt: Inputs.range([0, 100], {label: "marginTop", step: 1}),
-	mr: Inputs.range([0, 100], {label: "marginRight", step: 1}),
-	mb: Inputs.range([0, 100], {label: "marginBottom", step: 1}),
-	ml: Inputs.range([0, 100], {label: "marginLeft", step: 1}),
+	mt:  Inputs.range([0, 100], {label: "marginTop", step: 1}),
+	mr:  Inputs.range([0, 100], {label: "marginRight", step: 1}),
+	mb:  Inputs.range([0, 100], {label: "marginBottom", step: 1}),
+	ml:  Inputs.range([0, 100], {label: "marginLeft", step: 1}),
 })
 )}
 
-function _4(Plot,settings,data){return(
-Plot.plot({ 
-  marginTop: settings.mt,
-	marginRight: settings.mr,
-	marginBottom: settings.mb,
-	marginLeft: settings.ml,
-	y: {grid: true, label: "count"},
-	marks: [
-		Plot.rectY(data, Plot.binX({y:"count"}, { x:"Year", interval:1, fill:"Year", tip: true })),
+function _4(Plot,plot1,data){return(
+Plot.plot({  
+	marginTop: plot1.mt, 
+	marginRight: plot1.mr, 
+	marginBottom: plot1.mb, 
+	marginLeft: plot1.ml,   
+	y: {grid: true, label: "count"},  
+	marks: [    
+		Plot.rectY(data, Plot.binX({y:"count"}, { x:"Year", interval:1, fill:"Gender", tip: true })),    
 		Plot.gridY({ interval: 1, stroke: "white", strokeOpacity: 0.5 })
-	]
+	 ]
 })
 )}
 
@@ -39,8 +39,8 @@ export default function define(runtime, observer) {
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], _1);
   main.variable(observer("data")).define("data", ["FileAttachment"], _data);
-  main.variable(observer("viewof settings")).define("viewof settings", ["Inputs"], _settings);
-  main.variable(observer("settings")).define("settings", ["Generators", "viewof settings"], (G, _) => G.input(_));
-  main.variable(observer()).define(["Plot","settings","data"], _4);
+  main.variable(observer("viewof plot1")).define("viewof plot1", ["Inputs"], _plot1);
+  main.variable(observer("plot1")).define("plot1", ["Generators", "viewof plot1"], (G, _) => G.input(_));
+  main.variable(observer()).define(["Plot","plot1","data"], _4);
   return main;
 }
